d3bc11144072149b7dda4213f37eb45ea1233333
Richard Lirio <richardlirio@hotmail.com>
2025-04-22 09:53:25 -0300

    fix:fix tsconfig and iniciate nest app from zero

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { PrismaService } from "./prisma/prisma.service";
 
 @Module({
-  controllers: [AppController], //tudo que recebe requisições
-  providers: [AppService, PrismaService], //o resto é provider
+  controllers: [], //tudo que recebe requisições
+  providers: [PrismaService], //o resto é provider
 })
 export class AppModule {}
