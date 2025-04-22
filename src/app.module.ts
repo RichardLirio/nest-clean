1408bb9cadad327f5a97dbb9b79e8cc630623664
Richard Lirio <richardlirio@hotmail.com>
2025-04-22 11:27:07 -0300

    feat: create account controller created.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
+import { CreateAccountController } from "./controllers/create-account.controller";
 
 @Module({
-  controllers: [], //tudo que recebe requisições
+  controllers: [CreateAccountController], //tudo que recebe requisições
   providers: [PrismaService], //o resto é provider
 })
 export class AppModule {}
