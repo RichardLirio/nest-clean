63d8f271690d251b57685bedb275ea956888d352
Richard Lirio <richardlirio@hotmail.com>
2025-06-18 13:05:14 -0300

    Controller de listagem de perguntas.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { envSchema } from "./env";
 import { AuthModule } from "./auth/auth.module";
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
+import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { CreateQuestionController } from "./controllers/create-question.controll
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ], //tudo que recebe requisições, comunicação http
   providers: [PrismaService], //o resto é provider, modulos ou classes injetaveis dentro dos controllers
 })
