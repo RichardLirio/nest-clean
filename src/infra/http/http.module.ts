a7e87e76a5ddc81201bf50307b4455b4720a3728
Richard Lirio <richardlirio@hotmail.com>
2025-06-19 00:16:45 -0300

    Implementando repositórios do Prisma

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,15 +3,16 @@ import { CreateAccountController } from "./controllers/create-account.controller
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
 import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
-import { PrismaService } from "../prisma/prisma.service";
+import { PrismaService } from "../database/prisma/prisma.service";
+import { DatabaseModule } from "../database/database.module";
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ], //tudo que recebe requisições, comunicação http
-  providers: [PrismaService], //o resto é provider, modulos ou classes injetaveis dentro dos controllers
 })
 export class HttpModule {}
