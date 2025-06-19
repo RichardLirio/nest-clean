3b7bc5c18739e91c44cd1286eb72e5af903b1f4d
Richard Lirio <richardlirio@hotmail.com>
2025-06-18 21:05:48 -0300

    Criando teste e2e de busca de questions

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,39 @@
-test("1 plus 1", () => {
-  expect(1 + 1).toBe(2);
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+
+describe("Create account (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile(); //compila/roda aplicação somente de forma programatica para os testes
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+    await app.init();
+  });
+
+  test("[POST] /accounts", async () => {
+    const response = await request(app.getHttpServer()).post("/accounts").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(response.statusCode).toBe(201); //status code esperado no retorno da rota de criação de usuario
+
+    const userOnDatabase = prisma.user.findUnique({
+      where: {
+        email: "johndoe@example.com",
+      },
+    });
+
+    expect(userOnDatabase).toBeTruthy(); //verifica dentro do banco de dados se o usuario foi criado
+  });
 });
---
+++ src/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -0,0 +1,54 @@
+import { AppModule } from "@/app.module";
+import { PrismaService } from "@/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+
+describe("Fetch recent questions (E2E)", () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile(); //compila/roda aplicação somente de forma programatica para os testes
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
+    await app.init();
+  });
+
+  test("[GET] /questions", async () => {
+    const user = await prisma.user.create({
+      data: {
+        name: "John Doe",
+        email: "johndoe@example.com",
+        password: "123456",
+      },
+    }); //criando usuario antes de realizar o login
+
+    const accessToken = jwt.sign({ sub: user.id });
+
+    const response = await request(app.getHttpServer())
+      .get("/questions")
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        tittle: "New question",
+        content: "Question content",
+      });
+
+    expect(response.statusCode).toBe(201); //status code esperado no retorno da rota de criação de usuario
+
+    const questionOnDatabase = prisma.question.findFirst({
+      where: {
+        tittle: "New question",
+      },
+    });
+
+    expect(questionOnDatabase).toBeTruthy(); //verifica dentro do banco de dados se o usuario foi criado
+  });
+});
